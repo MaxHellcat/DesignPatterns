2f18d07f3fd7c4b9f54b96af0877c931b98572dd
Max Reshetey <maxim.reshetey@gmail.com>
2016-12-04 12:34:45 +0300

    Initial Commit

---
+++ DesignPatterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by Max Reshetey on 04/12/2016.
+//  Copyright © 2016 Max Reshetey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
